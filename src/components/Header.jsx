93aad6635191a49c058314167a255b42c849c967
afmurillo97 <afmurillo97@gmail.com>
2022-09-12 20:55:17 -0500

    include dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
+import "../styles/Header.css";
+import Characters from './Characters';
+
+export const DarkModeContext = createContext();
 
 const Header = () => {
+
     const [darkMode, setDarkMode] = useState(false);
 
     const handleClick = () => {
@@ -8,11 +13,15 @@ const Header = () => {
     }
 
   return (
-    <div className="Header">
-        <h1>ReactHooks</h1>
-        <button type='button' onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
-        <button type='button' onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Dark Mode 2' : 'Light Mode 2'}</button>
-    </div>
+    <DarkModeContext.Provider value={darkMode}>
+      <div className="Header">
+        <h1 className={ darkMode ? `TitleDark` : `TitleLight` }>ReactHooks</h1>
+        <button type='button' className={ darkMode ? `HeaderButton Light` : `HeaderButton Dark` } onClick={handleClick}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
+        {/* {another way to do this...} */}
+        {/* <button type='button' onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Dark Mode 2' : 'Light Mode 2'}</button> */}
+      </div>
+      <Characters/>
+    </DarkModeContext.Provider>
   )
 };
 
