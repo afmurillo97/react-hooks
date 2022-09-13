255e1de633162b29caee800f004be71397ea09dc
afmurillo97 <afmurillo97@gmail.com>
2022-09-12 20:54:46 -0500

    fetch more info and add classes

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
+import React, { useState, useEffect, useContext } from 'react';
+import '../styles/Characters.css';
+import { DarkModeContext } from './Header';
 
 const Characters = () => {
-
+    const darkMode = useContext(DarkModeContext);
     const [characters, setCharacters] = useState([]);
-    console.log(characters);
+    // console.log(darkMode);
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -13,9 +14,18 @@ const Characters = () => {
     }, [])
 
   return (
-    <div className="Characters">
+    <div className={ darkMode ? `Characters Dark` : `Characters Light` }>
         {characters.map((character, idx) => (
-            <h2 key={idx}>{character.name}</h2>
+          <div className='Characters-Card' key={idx}>
+            <h2 className={ darkMode ? `TitleDark` : `TitleLight` }>{character.name}</h2>
+            <img src={character.image} alt={character.name} className="Character-image"/>
+            <ul className={ darkMode ? `Character-data TitleDark` : `Character-data TitleLight` }>
+              <li><strong>Status:</strong> {character.status}</li>
+              <li><strong>Species:</strong>  {character.species}</li>
+              <li><strong>Gender:</strong>  {character.gender}</li>
+              <li><strong>Origin:</strong>  {character.origin.name}</li>
+            </ul>
+          </div>
         ))}
     </div>
   );
